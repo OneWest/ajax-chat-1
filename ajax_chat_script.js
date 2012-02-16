b8bd3a093da59da281934d5197d17fce2c6983d6
Helena 'Adei' Josol <helena.josol@gmail.com>
2012-02-16 17:21:01 +0000

    Initial commit

---
+++ ajax_chat_script.js
@@ -0,0 +1,77 @@
+function init() {
+	if (window.XMLHttpRequest) {
+		request = new XMLHttpRequest();
+	} else if (window.ActiveXObject) {
+		request = new ActiveXObject('Microsoft.XMLHTTP');
+	}
+
+	messageWindow = document.getElementById('messageWindow');
+	messageField = document.getElementById('messageField');
+	button = document.getElementById('sendMessageButton');
+
+	messageWindow.setAttribute('disabled', 'disabled');
+	button.setAttribute('disabled', 'disabled');
+
+	messageField.addEventListener('keyup', changeButtonState);
+	button.addEventListener('click', sendMessage);
+
+	retrieveMessages();
+
+	// setInterval("fetchNewMessages()", 1000);
+}
+
+function setAjaxRequest() {
+	request.open('post', 'ajax_chat_server_script.php', true);
+	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+}
+
+function changeButtonState() {
+	if (messageField.value && button.hasAttribute('disabled')) {
+		button.removeAttribute('disabled');
+	} else if (!messageField.value && !button.hasAttribute('disabled')) {
+		button.setAttribute('disabled', 'disabled');
+	}
+}
+
+function retrieveMessages() {
+	setAjaxRequest();
+	request.onreadystatechange = getMessagesRequest;
+	request.send('action=retrieve');
+}
+
+function fetchNewMessages() {
+	setAjaxRequest();
+	request.onreadystatechange = getMessagesRequest;
+	request.send('action=fetch&length=' + encodeURIComponent(messagesLength));
+}
+
+function getMessagesRequest() {
+	if (request.readyState == 4 && request.status == 200) {
+		var messagesArray = JSON.parse(request.responseText);
+		messagesLength = --messagesArray.length;
+
+		for (var i = 0; i < messagesLength; i++) {
+			messageWindow.value += messagesArray[i] + '\n';
+		}
+	}
+}
+
+function sendMessage() {
+	var message = messageField.value;
+
+	messageField.value = '';
+	changeButtonState();
+
+	setAjaxRequest();
+	request.onreadystatechange = sendMessageRequest;
+	request.send('action=send&message=' + encodeURIComponent(message));
+}
+
+function sendMessageRequest() {
+	if (request.readyState == 4 && request.status == 200) {
+		messagesLength++;
+		messageWindow.value += request.responseText + '\n';
+	}
+}
+
+document.addEventListener('DOMContentLoaded', init);
---
+++ ajax_chat_server_script.php
@@ -0,0 +1,32 @@
+<?php
+
+$action = $_POST['action'];
+
+if ($action == 'retrieve') {
+	$messages = explode('\n', file_get_contents('ajax_chat_message_log.txt'));
+
+	echo json_encode($messages);
+} else if ($action == 'fetch') {
+	$messages_length = $_POST['length'];
+
+	$log_messages = explode('\n', file_get_contents('ajax_chat_message_log.txt'));
+	$log_messages_length = count($log_messages);
+
+	if ($messages_length < --$log_messages_length) {
+		$message_difference = array();
+
+		for ($i = $messages_length; $i < $log_messages_length; $i++) {
+			$message_difference[] = $log_messages[$i];
+		}
+
+		echo json_encode($message_difference);
+	}
+} else if ($action == 'send') {
+	$message = $_POST['message'];
+
+	$log = fopen('ajax_chat_message_log.txt', 'a');
+	fwrite($log, $message.'\n');
+	fclose($log);
+
+	echo $message;
+}
