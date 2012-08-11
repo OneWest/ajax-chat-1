f497ade1dcf65b4a41c88a438c864cd7d98a924c
Helena Josol <helena.josol@gmail.com>
2012-08-11 20:52:11 +0100

    Fix code style

--- ajax_chat_server_script.php
+++ ajax_chat_server_script.php
@@ -1,9 +1,9 @@
 <?php
 
-function ensure_log_exists($log_file) {
-	if (!file_exists($log_file)) {
-		$log = fopen($log_file, 'w');
-		fclose($log);
+function ensure_log_exists( $log_file ) {
+	if ( !file_exists( $log_file ) ) {
+		$log = fopen( $log_file, 'w' );
+		fclose( $log );
 	}
 }
 
@@ -12,13 +12,13 @@ function login_user() {
 	$username = $_POST['username'];
 	$delimiter = $_POST['delimiter'];
 
-	ensure_log_exists($users_log);
-	$users = explode($delimiter, file_get_contents($users_log));
-	$empty_user_index = count($users) - 1;
-	unset($users[$empty_user_index]);
+	ensure_log_exists( $users_log );
+	$users = explode( $delimiter, file_get_contents( $users_log ) );
+	$empty_user_index = count( $users ) - 1;
+	unset( $users[$empty_user_index] );
 
-	if (in_array($username, $users) && array_search($username, $users) % 2 == 0) {
-		$user_password_index = array_search($username, $users) + 1;
+	if ( in_array( $username, $users ) && array_search( $username, $users ) % 2 == 0 ) {
+		$user_password_index = array_search( $username, $users ) + 1;
 		echo $users[$user_password_index];
 	} else {
 		echo '';
@@ -31,42 +31,42 @@ function register_user() {
 	$password = $_POST['password'];
 	$delimiter = $_POST['delimiter'];
 
-	ensure_log_exists($users_log);
-	$log = fopen($users_log, 'a');
-	fwrite($log, $username.$delimiter.$password.$delimiter);
-	fclose($log);
+	ensure_log_exists( $users_log );
+	$log = fopen( $users_log, 'a' );
+	fwrite( $log, $username . $delimiter . $password . $delimiter );
+	fclose( $log );
 }
 
 function retrieve_messages() {
 	$message_log = $_POST['message_log'];
 	$delimiter = $_POST['delimiter'];
 
-	ensure_log_exists($message_log);
-	$messages = explode($delimiter, file_get_contents($message_log));
-	$empty_message_index = count($messages) - 1;
-	unset($messages[$empty_message_index]);
+	ensure_log_exists( $message_log );
+	$messages = explode( $delimiter, file_get_contents( $message_log ) );
+	$empty_message_index = count( $messages ) - 1;
+	unset( $messages[$empty_message_index] );
 
-	if (!empty($messages)) {
-		echo json_encode($messages);
+	if ( !empty( $messages ) ) {
+		echo json_encode( $messages );
 	}
 }
 
 function fetch_messages() {
 	$message_log = $_POST['message_log'];
-	$length = intval($_POST['length']);
+	$length = intval( $_POST['length'] );
 	$delimiter = $_POST['delimiter'];
 
-	ensure_log_exists($message_log);
-	$log_messages = explode($delimiter, file_get_contents($message_log));
-	$log_length = count($log_messages) - 1;
+	ensure_log_exists( $message_log );
+	$log_messages = explode( $delimiter, file_get_contents( $message_log ) );
+	$log_length = count( $log_messages ) - 1;
 
 	$new_messages = array();
-	for ($i = $length; $i < $log_length; $i++) {
+	for ( $i = $length; $i < $log_length; $i++ ) {
 		$new_messages[] = $log_messages[$i];
 	}
 
-	if (!empty($new_messages)) {
-		echo json_encode($new_messages);
+	if ( !empty( $new_messages ) ) {
+		echo json_encode( $new_messages );
 	}
 }
 
@@ -76,13 +76,13 @@ function send_message() {
 	$message = $_POST['message'];
 	$delimiter = $_POST['delimiter'];
 
-	ensure_log_exists($message_log);
-	$log = fopen($message_log, 'a');
-	fwrite($log, '['.$username.'] '.$message.$delimiter);
-	fclose($log);
+	ensure_log_exists( $message_log );
+	$log = fopen( $message_log, 'a' );
+	fwrite( $log, '[' . $username . '] ' . $message . $delimiter );
+	fclose( $log );
 }
 
-switch ($_POST['action']) {
+switch ( $_POST['action'] ) {
 	case 'login':
 		login_user();
 		break;
@@ -99,3 +99,5 @@ switch ($_POST['action']) {
 		send_message();
 		break;
 }
+
+?>
